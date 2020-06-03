4630d5cd57c28ab7353d1a966ca45ce63a7cd382
Jiahe Chen <jc5348@columbia.edu>
2020-06-02 23:44:12 -0400

    fix the merge record in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -139,27 +139,10 @@ EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-<<<<<<< HEAD
-=======
+
 
 # TODO: change if you use for your personal blog
 DEFAULT_FROM_EMAIL = 'Django Boards <noreply@jhchen.cc>'
 EMAIL_SUBJECT_PREFIX = '[Django Board]'
 
 
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> e10eefb704e06eb961904298704008b3480b0958
-
-# TODO: change if you use for your personal blog
-DEFAULT_FROM_EMAIL = 'Django Boards <noreply@jhchen.cc>'
-EMAIL_SUBJECT_PREFIX = '[Django Board]'
